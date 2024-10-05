3b4c747b432e79ce49d041bd792af6be93b83137
ThanhLong1601 <72308447+ThanhLong1601@users.noreply.github.com>
2024-10-05 11:51:25 +0700

    Fix entity User

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -7,27 +7,14 @@ export class User {
   id!: number;
 
   @Column()
-  @IsNotEmpty({message: 'Name không được để trống'})
-  @IsString({message: 'Name phải là một chuỗi ký tự'})
-  @MaxLength(50, {message: 'Name không được vượt quá 50 ký tự'})
-  @Matches(/^[A-Za-z\s]+$/, {message: 'Name không được chứa số'})
   name!: string;
 
   @Column()
-  @IsNotEmpty({message: 'Phone không được để trống'})
-  @Matches(/^\+?\d{10,15}$/, { message: 'Phone phải bắt đầu bằng dấu + (nếu có) và có từ 10 đến 15 số' })
   phone!: string;
 
   @Column({unique: true})
-  @IsNotEmpty({message: 'Email không được để trống'})
-  @IsEmail({},{message: 'Email phải đúng định dạng hợp lệ'})
   email!: string; 
 
   @Column({unique: true})
-  @IsNotEmpty({ message: 'Password không được để trống' })
-  @Length(4, 10, { message: 'Password phải có độ dài từ 4 đến 10 ký tự' })
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, { 
-    message: 'Password phải chứa ít nhất một chữ thường, một chữ hoa và một số' 
-  })
   password!: string;
 }
