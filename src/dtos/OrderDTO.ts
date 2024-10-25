d922d512d4ab4a57ec4ed1c874c4165f275c9947
Thanh Long <thanhlong160100@gmail.com>
2024-10-25 11:05:05 +0700

    Add column id,status for OrderDTO, add status for OrderSchema

--- src/dtos/OrderDTO.ts
+++ src/dtos/OrderDTO.ts
@@ -1,10 +1,13 @@
 import { OrderItemDTO } from "./OrderItemDTO";
+import { OrderStatus } from "../entities/OrderEntity";
 
 export type OrderDTO = {
+    id: number;
     userId: number;
     storeId: number;
     orderItems: OrderItemDTO[];
     shipping_address: string;
     discount: number;
+    status: OrderStatus;
     total_price: number;
   };
--- src/validations/orderSchema.ts
+++ src/validations/orderSchema.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 import { orderItemSchema } from "./orderItemSchema";
+import { OrderStatus } from "../entities/OrderEntity";
 
 export const orderSchema = z.object({
   userId: z.number().min(1, "UserID can not be empty"),
   storeId: z.number().min(1, "StoreID can not be empty"),
   shipping_address: z.string().min(1, "Shipping address is required"),
   discount: z.number().min(0).max(100, "Discount must be between 0 and 100").default(0),
+  status: z.enum([OrderStatus.INIT, OrderStatus.PENDING, OrderStatus.SHIPPED, OrderStatus.DELIVERED, OrderStatus.CANCELLED, OrderStatus.COMPLETED]).default(OrderStatus.INIT),
   orderItems: z.array(orderItemSchema)
 });
